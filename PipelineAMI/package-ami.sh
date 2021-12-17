1860dbed50a473fa2501550dfc8379c5aed3d822
Adalberto Teixeira <adalberto.teixeirajr@outlook.com>
2021-12-17 20:57:06 +0000

    Adicionar tag de versão e atualizar geração da AMI

--- PipelineAMI/package-ami.sh
+++ PipelineAMI/package-ami.sh
@@ -1,5 +1,5 @@
 
-# VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
+VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 cd PipelineAMI/ami
 # cd terraform
@@ -7,4 +7,4 @@ RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed 
 
 cd ../ami
 terraform init
-TF_VAR_versao=1 TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
+TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
