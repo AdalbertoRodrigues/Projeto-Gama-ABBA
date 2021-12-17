bce5ff3799645b2af548de90a2487c9c64410ddb
AdalbertoRodrigues <31300400+AdalbertoRodrigues@users.noreply.github.com>
2021-12-17 17:00:39 -0300

    Atualizar script ami

--- PipelineAMI/package-ami.sh
+++ PipelineAMI/package-ami.sh
@@ -1,10 +1,10 @@
 
-VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
+# VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 # cd PipelineAMI/ami
-cd ami
+cd terraform
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 cd ../ami
 terraform init
-TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
+TF_VAR_versao=1 TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
