367ae38852b0543ba3fbd181f89b4d3c40387abc
bruno roman <brunoroman05@gmail.com>
2021-12-18 00:46:19 +0000

    rede privada

--- 01-Rede-Isolada/terraform/outputs.tf
+++ 01-Rede-Isolada/terraform/outputs.tf
@@ -3,6 +3,7 @@ output "rede" {
     "VPC : ${aws_vpc.vpc_abba.id}",
     "SubnetPub1a: ${aws_subnet.abba_subnetPub_1a.id}",
     "SubnetPub1b: ${aws_subnet.abba_subnetPub_1b.id}",
-    "SubnetPub1c: ${aws_subnet.abba_subnetPub_1c.id}"
+    "SubnetPub1c: ${aws_subnet.abba_subnetPub_1c.id}",
+    "SubnetPriv_1a: ${aws_subnet.abba_subnetPriv_1a.id}"
   ]
 }
--- 01-Rede-Isolada/terraform/route_association.tf
+++ 01-Rede-Isolada/terraform/route_association.tf
@@ -38,29 +38,29 @@ resource "aws_route_table_association" "abba_rtpub_snetc"  {
 
 # route tables private
 
-# resource "aws_route_table" "abba_routePriva" {
-#   vpc_id = aws_vpc.vpc_abba.id
-#   route = [
-#       {
-#         carrier_gateway_id         = ""
-#         cidr_block                 = "0.0.0.0/0"
-#         destination_prefix_list_id = ""
-#         egress_only_gateway_id     = ""
-#         gateway_id                 = ""
-#         instance_id                = ""
-#         ipv6_cidr_block            = ""
-#         local_gateway_id           = ""
-#         nat_gateway_id             = aws_nat_gateway.abba_NAT1a.id
-#         network_interface_id       = ""
-#         transit_gateway_id         = ""
-#         vpc_endpoint_id            = ""
-#         vpc_peering_connection_id  = ""
-#       }
-#   ]
-#   tags = {
-#     Name = "abba_routePriva"
-#   }
-# }
+resource "aws_route_table" "abba_routePriva" {
+  vpc_id = aws_vpc.vpc_abba.id
+  route = [
+      {
+        carrier_gateway_id         = ""
+        cidr_block                 = "0.0.0.0/0"
+        destination_prefix_list_id = ""
+        egress_only_gateway_id     = ""
+        gateway_id                 = ""
+        instance_id                = ""
+        ipv6_cidr_block            = ""
+        local_gateway_id           = ""
+        nat_gateway_id             = aws_nat_gateway.abba_NAT1a.id
+        network_interface_id       = ""
+        transit_gateway_id         = ""
+        vpc_endpoint_id            = ""
+        vpc_peering_connection_id  = ""
+      }
+  ]
+  tags = {
+    Name = "abba_routePriva"
+  }
+}
 # resource "aws_route_table" "abba_routePrivb" {
 #   vpc_id = aws_vpc.vpc_abba.id
 #   route = [
@@ -109,10 +109,10 @@ resource "aws_route_table_association" "abba_rtpub_snetc"  {
 #   }
 # }
 
-# resource "aws_route_table_association" "abba_rtPriv_Assoc1a" {
-#   subnet_id      = aws_subnet.abba_subnetPriv_1a.id
-#   route_table_id = aws_route_table.abba_routePriva.id
-# }
+resource "aws_route_table_association" "abba_rtPriv_Assoc1a" {
+  subnet_id      = aws_subnet.abba_subnetPriv_1a.id
+  route_table_id = aws_route_table.abba_routePriva.id
+}
 # resource "aws_route_table_association" "abba_rtPriv_Assoc1b" {
 #   subnet_id      = aws_subnet.abba_subnetPriv_1b.id
 #   route_table_id = aws_route_table.abba_routePrivb.id
