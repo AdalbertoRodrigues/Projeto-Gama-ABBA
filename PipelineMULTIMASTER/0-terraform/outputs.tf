a2fb324a8841793c8d2071df9e440967c9711d77
BrunoRoman <brunoroman05@gmail.com>
2021-12-19 16:17:56 +0000

    public ip master

--- PipelineMULTIMASTER/0-terraform/outputs.tf
+++ PipelineMULTIMASTER/0-terraform/outputs.tf
@@ -5,6 +5,13 @@ output "k8s-masters" {
   ]
 }
 
+output "k8s-masters-public" {
+  value = [
+    for key, item in aws_instance.k8s_masters :
+      "k8s-master ${key+1} - ${item.public_ip} - ssh -i ~/key.pem ubuntu@${item.public_ip} -o ServerAliveInterval=60"
+  ]
+}
+
 output "output-k8s_workers" {
   value = [
     for key, item in aws_instance.k8s_workers :
